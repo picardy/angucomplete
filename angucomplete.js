8c8648230353931a5c5a04efb283226f26a5cb8c
Paul Marbach <fastfrwrd@gmail.com>
2014-07-09 23:45:16 -0500

    support proper syntax for minified app

--- angucomplete.js
+++ angucomplete.js
@@ -5,7 +5,7 @@
  */
 
 angular.module('angucomplete', [] )
-    .directive('angucomplete', function ($parse, $http, $sce, $timeout) {
+    .directive('angucomplete', ['$parse', '$http', '$sce', '$timeout', function ($parse, $http, $sce, $timeout) {
 
     return {
         restrict: 'EA',
@@ -271,5 +271,5 @@ angular.module('angucomplete', [] )
 
         }
     };
-});
+}]);
 
